1577417f22503c3852788345ca7d2dc294d4e4d5
jaymjordan <jaymjordan@gmail.com>
2016-01-07 14:56:17 -0700

    Display Posts

--- client/common.js
+++ client/common.js
@@ -2,5 +2,10 @@
     AccountsEntry.config({
       homeRoute: '/',           
       dashboardRoute: '/' 
+      passwordSignupFields: 'USERNAME_AND_EMAIL'
     });
+
+    Accounts.ui.config({
+      passwordSignupFields: 'USERNAME_AND_EMAIL'
+    })
   });
